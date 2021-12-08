d8a8f5a6020ce9d239a190c568126049ead3e7e9
MG <codewritter.1@gmail.com>
2021-12-09 00:38:04 +0530

    V.1.1

--- __dic__.py
+++ __dic__.py
@@ -1,1 +1,11 @@
-data = {}
+data = {"friend" : "Dost"}
+data["Matang Mehra"] = "The great Indian rishi."
+data["Hello"] = "The way of meeting to anyone."
+data["Mother"] = "Who gives birth to you."
+data["God"] = "Who made us or who made this world."
+data["Programmer"] = "who make programs by coding it."
+data["Big"] = "Sabse Barda"
+data["Pot"] = "Matka"
+data["Got"] = "Bakari"
+data["Balla"] = "Bat"
+data["Bat"] = "Balla"
--- main.py
+++ main.py
@@ -1,3 +1,51 @@
 #Dictonery
+from __dic__ import data
+# l = data
 
-print("Welcome To Master's Dictonery")
+print("""
+        .........................   .................
+        :::::::::::::::::::::::::   :::::::::::::::::
+        ....    .......     .....   ....       ::::::
+        ....    .......     .....   ....         ::::
+        ....    .......     .....   ....          :::
+        ....    .......     .....   ....          :::
+        ....                .....   ....          :::
+        ....                .....   ....          :::
+        ....                .....   ....         ::::
+        ....                .....   ....       ::::::
+        ::::                :::::   :::::::::::::::::
+        """)
+
+print("\t\tWelcome To Master's Dictonery")
+
+while(1):
+    take = input("\nEnter the word here : ")
+
+    crt = open("input_check.txt", "w")
+    crt.write(take)
+    crt.close()
+
+    opn = open("input_check.txt", "r")
+    rd = opn.read()
+    opn.close()
+
+    if ("0000" in rd):
+        exit()
+    elif (take in data):
+        print(f"\n Meaning : {data[take]}")
+    elif (take not in data):
+        print("Meaning not found.")
+        while(1):
+            ad = (input("Type 'add' to add the word in dictonery or 'continue' to go forward : "))
+            if (ad == "add" or ad  == "Add"):
+                add_word = input("Enter the Word : ")
+                add_meaning = input("\nEnter the meaning : ")
+                opn = open("__dic__.py","a")
+                rd = opn.write(f'data["{add_word}"] = "{add_meaning}"\n')
+                opn.close()
+                print("Word is added to the dictonery. \nThe Changes will take place after the restart of Program.")
+            elif (ad == "continue" or ad == "Continue"):
+                print("Sorry for not giving your answer.")
+                break
+    continue
+
